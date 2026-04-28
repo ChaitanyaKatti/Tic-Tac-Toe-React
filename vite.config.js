c22773591632904db3e502bb52ed8b3572207c63
ChaitanyaKatti <kattichaitanya@gmail.com>
2026-04-29 01:15:23 +0530

    Minified with terser

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
-    minify: 'esbuild', 
+    minify: 'terser', 
     terserOptions: {
       compress: {
         drop_console: true,
