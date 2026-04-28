767a11f18aa706c086e023d273f236c02cf51c78
ChaitanyaKatti <kattichaitanya@gmail.com>
2026-04-29 01:14:08 +0530

    Minified

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+    minify: 'esbuild', 
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+    cssMinify: 'lightningcss' 
+  },
   plugins: [
     react(),
     tailwindcss(),
